45b675b62048362ecf9b23c344505f22f429440a
yasser_sayed <yassersayed952@gmail.com>
2024-06-01 17:20:37 +0200

    prod static img test

--- app/product/[prodId]/ProdImgCC.jsx
+++ app/product/[prodId]/ProdImgCC.jsx
@@ -3,6 +3,7 @@ import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import NextJsImageCC from "./NextImageCC";
 import Image from "next/image";
+import img11 from "@/assets/slider-1.jpg";
 
 const ProdImgCC = ({ prodDet }) => {
   const [open, setOpen] = useState(false);
@@ -13,14 +14,14 @@ const ProdImgCC = ({ prodDet }) => {
         width={2000}
         height={1000}
         alt="image"
-        className="w-full h-[70%] cursor-zoom-in"
+        className="w-full h-[70%] cursor-zoom-in px-4"
         onClick={() => setOpen(true)}
       />
 
       <Lightbox
         open={open}
         close={() => setOpen(false)}
-        slides={prodDet?.images}
+        slides={[img11]}
         render={{ slide: NextJsImageCC }}
       />
     </div>
