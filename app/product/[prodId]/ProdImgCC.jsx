dfe25e976fb7a830e1f2ac499db4be0cec530e40
yasser_sayed <yassersayed952@gmail.com>
2024-06-01 16:31:13 +0200

    prod image error fix

--- app/product/[prodId]/ProdImgCC.jsx
+++ app/product/[prodId]/ProdImgCC.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import NextJsImageCC from "./NextImageCC";
 
 const ProdImgCC = ({ prodDet }) => {
+  const [open, setOpen] = useState(false);
   return (
     <div>
       <Lightbox
@@ -12,6 +13,8 @@ const ProdImgCC = ({ prodDet }) => {
         slides={prodDet?.images}
         render={{ slide: NextJsImageCC }}
       />
+
+      <button onClick={() => setOpen(true)}>tesst</button>
     </div>
   );
 };
