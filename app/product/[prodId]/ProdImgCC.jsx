32eac577204616911e21862e0d6c3c1f597cc41b
yasser_sayed <yassersayed952@gmail.com>
2024-06-01 16:39:29 +0200

    prod image onclick test

--- app/product/[prodId]/ProdImgCC.jsx
+++ app/product/[prodId]/ProdImgCC.jsx
@@ -2,19 +2,27 @@ import React, { useState } from "react";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import NextJsImageCC from "./NextImageCC";
+import Image from "next/image";
 
 const ProdImgCC = ({ prodDet }) => {
   const [open, setOpen] = useState(false);
   return (
     <div>
+      <Image
+        src={prodDet?.images[0]}
+        width={2000}
+        height={1000}
+        alt="image"
+        className="w-full h-4/5 cursor-zoom-in"
+        onClick={() => setOpen(true)}
+      />
+
       <Lightbox
         open={open}
         close={() => setOpen(false)}
         slides={prodDet?.images}
         render={{ slide: NextJsImageCC }}
       />
-
-      <button onClick={() => setOpen(true)}>tesst</button>
     </div>
   );
 };
