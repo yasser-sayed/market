c5662555994406a1294609ed9349d90a4ded387d
yasser_sayed <yassersayed952@gmail.com>
2024-06-01 17:52:40 +0200

    prod small imgs update

--- app/product/[prodId]/ProdImgCC.jsx
+++ app/product/[prodId]/ProdImgCC.jsx
@@ -3,13 +3,18 @@ import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import NextJsImageCC from "./NextImageCC";
 import Image from "next/image";
+import { Flex } from "@chakra-ui/react";
 
 const ProdImgCC = ({ prodDet }) => {
   const [open, setOpen] = useState(false);
   const [imgs, setImgs] = useState([]);
 
   useEffect(() => {
-    setImgs(prodDet?.images);
+    const newImgs = prodDet?.images.map((img) => {
+      return { src: img };
+    });
+
+    setImgs(newImgs);
   }, [prodDet]);
   return (
     <div>
@@ -21,6 +26,23 @@ const ProdImgCC = ({ prodDet }) => {
         className="w-full h-[70%] cursor-zoom-in px-4"
         onClick={() => setOpen(true)}
       />
+
+      <Flex
+        alignItems={"center"}
+        justifyContent={"center"}
+        gap={6}
+        flexWrap={"wrap"}
+      >
+        {prodDet?.images.map((img) => {
+          <Image
+            width={2000}
+            height={1000}
+            alt="image"
+            className="w-auto h-[30%] cursor-zoom-in px-4 hover:p-10"
+            onClick={() => setOpen(true)}
+          />;
+        })}
+      </Flex>
 
       <Lightbox
         open={open}
